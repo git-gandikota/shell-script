4243b1fed82c3e88877dd71f178feb2d6626c455
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 11:08:12 +0530

    data types

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTILLU" "MURARI")
+
+#size of above array is 3
+#index are 0,1,2
+#list always starts with 0.
+
+echo "First movie is: ${MOVIES[0]}"
+echo "First movie is: ${MOVIES[1]}"
+echo "First movie is: ${MOVIES[2]}"
