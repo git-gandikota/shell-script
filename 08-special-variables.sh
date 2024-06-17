53b2f741dee9eb2575c791b31a0379542408f348
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 11:27:31 +0530

    special variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,3 +5,4 @@ echo "Number of variables passed: $#" #to print nunber of variables passed
 echo "Script Name: $0" #to print script name
 echo "Current working directory: $PWD" #to print current working directory
 echo "Home directory of a current user: $HOME"
+echo "Which user is running this script: $USER"
