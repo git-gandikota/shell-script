cb7443f60b4e1df946f272172192f8d9a6db4cc5
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 10:52:24 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #here username in variable
+
+echo "Please enter password::"
+
+read PASSWORD
+
+echo "Username is: USERNAME, Password is: $PASSWORD"
