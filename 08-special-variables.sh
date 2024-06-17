247c497da336bb3362baf17694a2c2aedc73a12b
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 11:21:52 +0530

    special variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "All variavles: $@"
+echo "All variavles: $@" #to print all variables passed to script
+echo "Number of variables passed: $#" #to print nunber of variables passed
