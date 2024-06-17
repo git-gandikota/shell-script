62964ad1f1b5ca7c978851413d7cf1232194a9f9
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 10:36:49 +0530

    hello script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is again smaple script
+#!/bin/bash
+
+echo "Ramesh:: Hello Sureh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh:: I'm fine too. How is your work?"
+echo "Suresh:: Not bad. I'm thinking to upgrade to DevOps."
