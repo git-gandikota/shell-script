b89be5ac1cc7980be109f81adec92a3a15b825fe
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 17:55:46 +0530

    functions intialization

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,12 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "Exit status: $1"
-    echo "Whatare you doing: $2"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....Failed"
+        exit 1
+    else
+        echo "$2....suceed"
 }
 
 if [ $USERID -ne 0 ]
