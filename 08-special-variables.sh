ff93cbadb07596eeaef65e6f47e63b32e3833045
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 11:30:12 +0530

    special variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,3 +7,4 @@ echo "Current working directory: $PWD" #to print current working directory
 echo "Home directory of a current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
