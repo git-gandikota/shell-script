f66a26dbc42193810392c9cfb80c662bf306805d
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 11:10:25 +0530

    data types

--- 06-array.sh
+++ 06-array.sh
@@ -9,3 +9,5 @@ MOVIES=("RRR" "DJTILLU" "MURARI")
 echo "First movie is: ${MOVIES[0]}"
 echo "Second movie is: ${MOVIES[1]}"
 echo "Third movie is: ${MOVIES[2]}"
+
+echo "First movie is: ${MOVIES[@]}"
