f144c4dae461e3ba83f82f5631bdf67a6fca50ae
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 18:27:06 +0530

    functions intialization

--- 11-functions-2.sh
+++ 11-functions-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USERID=$(is -u)
-TIMESTAMP=$(echo date +%F-%H-%M-%S)
+USERID=$(ID -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/TMP/$0/$SCRIPT_NAME-$TIMESTAMP.log
 
