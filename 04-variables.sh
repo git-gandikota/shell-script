307824be44d7f8ba073abced516ce50ffea6b3d8
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 10:47:05 +0530

    hello script

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of variable
+PERSON1=$1
+PERSON2=$2
+
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I'm fine too. How is your work?"
+echo "$PERSON2:: Not bad. I'm thinking to upgrade to DevOps."
