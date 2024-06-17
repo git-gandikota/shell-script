dbfaf67c7ea4dc2d9a8a5e7a8b244973707171d6
git-gandikota <ganidkota.fou@gamil.com>
2024-06-17 18:19:25 +0530

    functions intialization

---
+++ 11-functions-2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID-$(is -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/TMP/$0/$SCRIPT_NAME-$TIMESTAMP.log
+
+#This all sentences are used to create a log file
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....Failed"
+        exit 1
+    else
+        echo "$2....suceed"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 #manually exit if error comes.
+else
+    echo "You are Super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
+
+echo "All your commands was successfull."
