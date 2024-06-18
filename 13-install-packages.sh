54f1af2b6b69c04fc090305c5453228a7d9aba80
git-gandikota <ganidkota.fou@gamil.com>
2024-06-18 10:28:25 +0530

    packages intialization

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R Failed $N"
+        exit 1
+    else
+        echo -e "$2....$G suceed $N"
+    fi
+}
+
 USERID=$(id -u)
+
 
 if [ $USERID -ne 0 ]
 then
@@ -13,4 +28,9 @@ fi
 for i in $@
 do
     echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed...Now it is skipping"
+    fi
 done
