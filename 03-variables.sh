27455ef4abf2d0dbc0587953c7aa1e9b9a356515
git-gandikota <ganidkota.fou@gamil.com>
2024-06-18 19:32:31 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#declaring of variable
 PERSON1=Ram #no space between = and value
 PERSON2=Raghava
 
+#referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I'm fine too. How is your work?"
